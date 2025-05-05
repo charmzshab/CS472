06f0ddd5bb0abb1ee5ea8165f91283063ae2f365
Lukyamuzi Shaban <kmharouna@gmail.com>
2025-05-05 11:05:04 -0500

    made changes to script

--- lab6/question3/script.js
+++ lab6/question3/script.js
@@ -1,1 +1,22 @@
 // question 3
+function Animal(name, speed) {
+  this.name = name;
+  this.speed = speed;
+}
+
+function Rabbit(name) {
+  Animal.call(this.name);
+}
+
+Object.setPrototypeOf(Rabbit.prototype, Animal.prototype);
+Object.setPrototypeOf(Rabbit, Animal);
+
+let rabbit1 = new Rabbit("brown rabbit");
+
+Animal.prototype.run = function (speed) {
+  this.speed = speed;
+};
+
+Animal.compareBySpeed = function (a1, a2) {
+  return a1.speed - a2.speed;
+};
